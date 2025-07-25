6010615942ec64a2f0d2273f559a6aa45a5c6f15
pVarsha2006 <varsha200646prajapati@gmail.com>
2025-07-25 09:22:57 +0530

    Add files via upload

---
+++ moter.java
@@ -0,0 +1,12 @@
+class moter
+{
+  moter(int a)
+{
+  System.out.println("parametrized constructor"); 
+}
+ 
+public static void main(String args[])
+{
+  moter m = new moter(15);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+}
+}
